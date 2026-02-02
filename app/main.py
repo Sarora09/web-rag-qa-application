705e90162d2e3aee2618c94858f4ea86f2d2e3f9
Sarora09 <52610194+Sarora09@users.noreply.github.com>
2026-02-01 22:39:55 -0500

    Added supervisor and docker file.

--- app/main.py
+++ app/main.py
@@ -7,14 +7,12 @@ def run_backend():
         subprocess.run(["uvicorn", "app.backend.web_rag_api:app", "--host", "localhost", "--port", "9999"], check=True)
     except Exception as e:
         print(e)
-        # raise CustomException("Internal Server Error", error_details=exception)
     
 def run_frontend():
     try:
         subprocess.run(["streamlit", "run", "app/frontend/index.py"], check=True)
     except Exception as e:
         print(e)
-        # raise CustomException("Internal Server Error", error_details=exception)
     
 if __name__ == "__main__":
     try:
@@ -23,4 +21,3 @@ if __name__ == "__main__":
         run_frontend()
     except Exception as e:
         print(e)
-        # raise CustomException("Internal Server Error", error_details=exception)
